a2ac7d57d64d72da0a1038e6782feb21f593e5e6
NIHARIKA-SUREDDI <niharika.sureddi@gmail.com>
2024-12-26 14:22:56 +0530

    second

---
+++ baseUrl.js
@@ -0,0 +1,1 @@
+export const baseUrl = "https://cautious-sand-sailfish.glitch.me/user";
---
+++ footer.js
@@ -0,0 +1,11 @@
+const footer = () => {
+    let card = `<div>
+         
+          <p>&copy; 2024 Mini TODO App</p>
+      </div>`;
+  
+    document.getElementById("footer").innerHTML = card;
+  };
+  
+  footer();
+  
